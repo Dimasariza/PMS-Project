d9d7dda7a954beb7b79f1f4d520feef810978433
BateraGames <BateraGames180@gmail.com>
2023-04-19 03:01:19 +0700

    Add login page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,67 @@
-import {
-  Typography,
-  Box,
-  Card,
-  Container,
-  Button,
-  styled
-} from '@mui/material';
+import { Typography, Box, Card, Container, Button, styled } from '@mui/material';
 import BaseLayout from 'src/layouts/BaseLayout';
-
 import Link from 'src/components/Link';
 import Head from 'next/head';
-
-import Logo from 'src/components/LogoSign';
-import Hero from 'src/content/Overview/Hero';
-
-const HeaderWrapper = styled(Card)(
-  ({ theme }) => `
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: ${theme.spacing(10)};
-  margin-bottom: ${theme.spacing(10)};
-`
-);
+import TextField from '@mui/material/TextField';
 
 const OverviewWrapper = styled(Box)(
   ({ theme }) => `
     overflow: auto;
     background: ${theme.palette.common.white};
     flex: 1;
-    overflow-x: hidden;
-`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `
 );
 
 function Overview() {
   return (
     <OverviewWrapper>
       <Head>
-        <title>Tokyo Free Black NextJS Javascript Admin Dashboard</title>
+        <title>Batera PMS</title>
       </Head>
-      <HeaderWrapper>
-        <Container maxWidth="lg">
-          <Box display="flex" alignItems="center">
-            <Logo />
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              flex={1}
+      
+      <Box sx={{ mt: 4 }}>
+        <Container maxWidth="sm">
+          <Box display="flex" flexDirection="column" gap={2} sx={{ mt: 4 }}>
+            <img
+              src="/static/images/logo/logo-batera.svg"
+              alt="Javascript"
+            />
+            <TextField
+              sx={{ width: '100%' }}
+              required
+              id="outlined-required"
+              label="Username"
+            />
+            <TextField
+              sx={{ width: '100%' }}
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <Button
+              sx={{ width: '100%' }}
+              component={Link}
+              href="/dashboards/tasks"
+              variant="contained"
             >
-              <Box />
-              <Box>
-                <Button
-                  component={Link}
-                  href="/dashboards/tasks"
-                  variant="contained"
-                  sx={{ ml: 2 }}
-                >
-                  Live Preview
-                </Button>
-              </Box>
-            </Box>
+              Login
+            </Button>
+            <Button
+              sx={{ width: '100%' }}
+              component={Link}
+              href="/dashboards/tasks"
+              variant="contained"
+            >
+              Examples Preview
+            </Button>
           </Box>
         </Container>
-      </HeaderWrapper>
-      <Hero />
-      <Container maxWidth="lg" sx={{ mt: 8 }}>
-        <Typography textAlign="center" variant="subtitle1">
-          Crafted by{' '}
-          <Link
-            href="https://bloomui.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BloomUI.com
-          </Link>
-        </Typography>
-      </Container>
+      </Box>
     </OverviewWrapper>
   );
 }
