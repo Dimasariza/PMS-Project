79decb92c0604d2ebc0e41d3b2d003c254a5d196
Dimasariza <roganda.sihombing11@gmail.com>
2023-04-26 02:12:56 +0700

    change button url

--- src/layouts/SidebarLayout-Batera/Sidebar/SidebarMenu/index.js
+++ src/layouts/SidebarLayout-Batera/Sidebar/SidebarMenu/index.js
@@ -223,7 +223,7 @@ function SidebarMenu() {
           <SubMenuWrapper>
             <List component="div">
               <ListItem component="div">
-                <NextLink href="/batera/dashboards/tasks" passHref>
+                <NextLink href="./batera/dashboards/tasks" passHref>
                   <Button
                     className={
                       currentRoute.includes('/batera/dashboards/tasks') ? 'active' : ''
