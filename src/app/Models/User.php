0f980852322439e5b582b07dd901ae7e590bf2a7
samuelsih <samuel.205025@mhs.its.ac.id>
2023-05-01 03:08:57 +0700

    feat: department soft delete

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
      */
     public function department(): BelongsTo
     {
-        return $this->belongsTo(Department::class, 'department_id');
+        return $this->belongsTo(Department::class, 'department_id')->withTrashed();
     }
 
 
