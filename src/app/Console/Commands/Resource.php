64a571851066c7881d13fde815870b6715827524
samuelsih <samuel.205025@mhs.its.ac.id>
2023-05-10 01:11:44 +0700

    chore: codegen for response

---
+++ src/app/Console/Commands/Resource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class Resource extends Command
+{
+/**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:res {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make bootstrapped api resource';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $model = $this->argument('model');
+
+        Artisan::call('make:resource', ['name' => "{$model}/{$model}Resource"]);
+        Artisan::call('make:resource', ['name' => "{$model}/{$model}CreatedResource"]);
+        Artisan::call('make:resource', ['name' => "{$model}/{$model}UpdatedResource"]);
+    }
+}
