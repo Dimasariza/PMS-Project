b9f068fab2cb6bd8ba0b480fd37311ff36a11ff3
samuelsih <samuel.205025@mhs.its.ac.id>
2023-05-11 11:47:05 +0700

    setup: job list soft delete

--- src/app/Models/JobList.php
+++ src/app/Models/JobList.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Ship;
 use App\Enums\JobType;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Department;
+use App\Models\TypingSystem;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobList extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * Indicates if the model should be timestamped.
@@ -45,4 +50,34 @@ class JobList extends Model
         'status',
         'level',
     ];
+
+    /**
+     * One job belongs to one department
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class, 'department_id')->withTrashed();
+    }
+
+    /**
+     * One job belongs to one ship
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function ship(): BelongsTo
+    {
+        return $this->belongsTo(Ship::class, 'ship_id')->withTrashed();
+    }
+
+    /**
+     * One job belongs to one typing system
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function typingSystem(): BelongsTo
+    {
+        return $this->belongsTo(TypingSystem::class, 'ship_id')->withTrashed();
+    }
 }
